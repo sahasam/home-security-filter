b8da8d3e6d73d705141b761093de165a9ec5f3dc
sahasrm <munamalasahas@gmail.com>
2020-04-20 19:52:00 -0500

    abstract model to odmodel class

--- person_detector.py
+++ person_detector.py
@@ -19,86 +19,67 @@ import sys
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-def runModel (detection_boxes, detection_scores, detection_classes, num_detections, frame_expanded) :
-    (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections], feed_dict={image_tensor: frame_expanded})
-    vis_util.visualize_boxes_and_labels_on_image_array(
-        frame,
-        np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
-        np.squeeze(scores),
-        category_index,
-        use_normalized_coordinates=True,
-        line_thickness=8,
-        min_score_thresh=0.40)
+class odmodel :
+    """
+    This class abstracts the tensorflow model so its main functions
+    can be used without concern for the lower-level implementation
+    """
+    def __init__ (self) :#, #q)
+    #    self.q = q
 
-    return (boxes, scores, classes, num)
+        self.MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+        self.CWD_PATH = os.getcwd()
 
+        self.PATH_TO_CKPT = os.path.join(self.CWD_PATH,self.MODEL_NAME,'frozen_inference_graph.pb')
+        self.PATH_TO_LABELS = os.path.join(self.CWD_PATH,'data','mscoco_label_map.pbtxt')
+        self.NUM_CLASSES = 90
 
-MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
-CWD_PATH = os.getcwd()
+        self.label_map = label_map_util.load_labelmap(self.PATH_TO_LABELS)
+        self.categories = label_map_util.convert_label_map_to_categories(self.label_map, max_num_classes=self.NUM_CLASSES, use_display_name=True)
+        self.category_index = label_map_util.create_category_index(self.categories)
 
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
-PATH_TO_LABELS = os.path.join(CWD_PATH,'data','mscoco_label_map.pbtxt')
-NUM_CLASSES = 90
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(self.PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+            self.sess = tf.Session(graph=detection_graph)
 
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+        self.image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-    sess = tf.Session(graph=detection_graph)
+        self.detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    def runModel (self, frame_expanded, frame) :
+        (boxes, scores, classes, num) = self.sess.run([self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections], feed_dict={self.image_tensor: frame_expanded})
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            frame,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            self.category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8,
+            min_score_thresh=0.40)
 
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        return (boxes, scores, classes, num)
 
-frame = cv2.imread('/home/pi/git/camFilter/utils/images/person-3.jpg', cv2.IMREAD_COLOR)
-frame.setflags(write=1)
-frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-frame_expanded = np.expand_dims(frame_rgb, axis=0)
+    def run_image (self, imagefile) :
+        frame = cv2.imread(imagefile, cv2.IMREAD_COLOR)
+        frame.setflags(write=1)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_expanded = np.expand_dims(frame_rgb, axis=0)
 
-(boxes, scores, classes, num) = runModel(detection_boxes, detection_scores, detection_classes, num_detections, frame_expanded) 
+        (boxes, scores, classes, num) = self.runModel(frame_expanded, frame)
+        return (boxes, scores, classes, num)
 
-
-
-# All the results have been drawn on the frame, so it's time to display it.
-cv2.imshow('Object detector', frame)
-
-cv2.waitKey(0)
-
-frame = cv2.imread('/home/pi/git/camFilter/utils/images/person-2.jpg', cv2.IMREAD_COLOR)
-frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-frame_expanded = np.expand_dims(frame_rgb, axis=0)
-
-
-(boxes, scores, classes, num) = sess.run(
-        [detection_boxes, detection_scores, detection_classes, num_detections], 
-        feed_dict={image_tensor: frame_expanded})
-
-vis_util.visualize_boxes_and_labels_on_image_array(
-    frame,
-    np.squeeze(boxes),
-    np.squeeze(classes).astype(np.int32),
-    np.squeeze(scores),
-    category_index,
-    use_normalized_coordinates=True,
-    line_thickness=8,
-    min_score_thresh=0.40)
-
-cv2.imshow('Object detector', frame)
-
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+if __name__ == "__main__" :
+    odm = odmodel()
+    odm.run_image('/home/pi/git/camFilter/utils/images/person-3.jpg')
 
